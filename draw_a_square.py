d9525232847957d96ef35110f1c8db4c66d5331d
Robert Dunn <robertdunn220@gmail.com>
2016-09-27 15:55:25 -0400

    Modified draw_a_square.py

--- draw_a_square.py
+++ draw_a_square.py
@@ -1,14 +1,18 @@
 from turtle import *
 
-def squareDraw():
-    for i in range(4):
-        forward(100)
-        left(90)
-    up()
-    forward(200)
-    down()
+def squareDraw(num_of_squares):
+    for x in range(num_of_squares):
+        for i in range(4):
+            forward(100)
+            left(90)
+        right(90)
+        up()
+        forward(150)
+        down()
 
-squareDraw()
-squareDraw()
+
+square_numbers = int(raw_input("How many squares do you want to draw? "))
+
+squareDraw(square_numbers)
 
 mainloop()
